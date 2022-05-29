8f0218d1f7037bbceeb54db00b79497955724e48
teu <daoquyanh2000@gmail.com>
2022-05-29 17:45:53 +0700

    push connectString

---
+++ Dao/ConnectString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyHoSo.Dao
+{
+    public class ConnectString
+    {
+        public  static string Setup()
+        {
+            return ConfigurationManager.ConnectionStrings["laptop"].ConnectionString;
+        }
+    }
+}
